eede446e9ae6a3a51c210e64697b0417ac670604
Diegoni <romain_diegoni@hotmail.com>
2017-03-30 20:44:58 +0100

    Add validation to prevent user to book an appointment when his slot finishs when the salon is closed

--- src/components/Calendar/src/components/BookingValidation.es6.jsx
+++ src/components/Calendar/src/components/BookingValidation.es6.jsx
@@ -1,3 +1,11 @@
+import moment from 'moment';
+
+function getEndOfWork(date, props) {
+  const currentDay = date.format('dddd').toLowerCase();
+  let endDate = moment(date.format('YYYY-MM-DD'));
+  return endDate.set('hour', props.timeSlice[currentDay].end);
+}
+
 export const required = value => value ? undefined : 'Required'
 
 export function validate(values, props) {
@@ -8,6 +16,12 @@ export function validate(values, props) {
 
     if (bookings.length > 0) {
         errors.service = 'You can\'t book this service. There is another booking at ' + bookings[0].startDate.format('HH:mm A');
+    }
+
+
+    const endOfWork = getEndOfWork(values.startDate, props);
+    if (values.endDate.isAfter(endOfWork)) {
+      errors.service = 'You can\'t book this service. The salon closes at ' + endOfWork.format('HH:mm A');
     }
 
     return errors;

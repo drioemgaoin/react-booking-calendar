08300b5b5e037b937f5ff37da35a5a31f2675db1
drioemgaoin <romain_diegoni@hotmail.com>
2017-03-31 16:51:55 +0100

    WIP: setup webpack production to be able to be deployed on heroku

---
+++ webpack.config.npm.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const CombineLoaders = require('webpack-combine-loaders')
+
+module.exports = {
+  context: path.join(__dirname),
+
+  devtool: 'cheap-module-source-map',
+
+  entry: './src/components/Calendar/index.js',
+
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.es6.jsx', '.scss']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: CombineLoaders([
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ])
+      },
+      {
+        test: /(\.js)|(\.jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['react', 'es2015', 'stage-0'] }
+      }
+    ]
+  },
+
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ],
+};

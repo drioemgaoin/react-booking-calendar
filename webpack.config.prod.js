7b4aa244bde9bb8bb7c9a6384e8c14cce3c701dd
Diegoni <romain_diegoni@hotmail.com>
2017-04-04 20:19:31 +0100

    Change heroku url to be https

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -94,7 +94,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
-        'API_URL': JSON.stringify('http://react-booking-calendar.herokuapp.com')
+        'API_URL': JSON.stringify('https://react-booking-calendar.herokuapp.com')
       }
     })
   ]

450cee9e979b8ac41b2d5bfa32a7fb4854008094
drioemgaoin <romain_diegoni@hotmail.com>
2017-04-04 13:41:12 +0100

    WIP: look into the cause of the popup reloading

--- src/components/Calendar/Calendar.es6.jsx
+++ src/components/Calendar/Calendar.es6.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
+import { Modal } from 'react-bootstrap';
 
 import CalendarHeader from './src/components/CalendarHeader';
 import CalendarBody from './src/components/CalendarBody';
-import Modal from './src/components/Modal';
+import BookingContainer from './src/components/BookingContainer';
+// import Modal from './src/components/Modal';
 
 import { initBookingsAction } from './src/actions/bookingActions';
 
@@ -25,21 +27,44 @@ class Calendar extends React.Component {
     dispatch(initBookingsAction(this.props.bookings));
   }
 
+  openModal(booking) {
+      this.setState({ showModal: true, booking: booking });
+  }
+
+  hideModal(e) {
+      if (e) {
+            e.preventDefault();
+      }
+
+      this.setState({ showModal: false, booking: {}});
+  }
+
   render() {
     return (
-        <div className='rbc-calendar'>
-          <Modal ref='modal'
-            body={this.props.body}
-            timeSlice={this.props.timeSlice}
-            bookings={this.props.bookings}
-            header='New Booking' />
+        <div className='rbc-calendar modal-container'>
+            <Modal
+              show={this.state.showModal}
+              onHide={this.hideModal}
+              container={this}
+              aria-labelledby='booking-modal'>
+              <Modal.Header closeButton>
+                <Modal.Title id='booking-modal'>New Booking</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                  <BookingContainer body={this.props.body}
+                                    bookings={this.props.bookings}
+                                    timeSlice={this.props.timeSlice}
+                                    initialValues={this.state.booking}
+                                    onClose={(e) => this.hideModal(e)} />
+              </Modal.Body>
+            </Modal>
 
           <CalendarHeader />
           <CalendarBody bookings={this.props.bookings}
                         timeSlot={this.props.timeSlot}
                         timeSlice={this.props.timeSlice}
                         isAdministrator={this.props.isAdministrator}
-                        onDayClick={(booking) => this.refs.modal.openModal(booking)} />
+                        onDayClick={(booking) => this.openModal(booking)} />
         </div>
     );
   }

ece82a7535b733999f6b67a1d69a1b5d464ebb0a
drioemgaoin <romain_diegoni@hotmail.com>
2017-04-20 10:17:59 +0100

    Fix the export component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import calendarActions from './components/Calendar/src/actions/calendarActions';
-import calendarReducer from './components/Calendar/src/reducers/calendarReducer'
-import Calendar from './components/Calendar/Calendar'
+import Calendar from './components/Calendar/Calendar';
 
-export {
-  // Constants
-  calendarActions,
-
-  // Reducer
-  calendarReducer,
-
-  // Component
-  Calendar
-};
+export default Calendar;
